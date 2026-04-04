acfc7f5b5ba6c9b1811ad060f75caeb357649f50
vera <elcheva_v_a@bk.ru>
2026-04-04 20:20:15 +0300

    -добавила CORS для работы бэка и фронта

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from backend.app.database import get_db
 from backend.app.routes import events
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(events.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main_page():
