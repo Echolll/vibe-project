6f0463ddc66259770263a1d4d1e5ed9003ff3163
Denis Shulga <denis02.de@live.ru>
2026-04-09 22:43:49 +0300

    Починка интеграционных тестов 2

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from backend.app.routes.routes import app
 from backend.app.database.database import Base, get_db
 from backend.app.database.event import Events
@@ -18,7 +17,7 @@ test_engine = create_engine(
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 
-Base.metadata.create_all(bind=test_engine)
+#Base.metadata.create_all(bind=test_engine)
 
 def override_get_db():
     db = TestingSessionLocal()
@@ -29,25 +28,20 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def client():
+    Base.metadata.create_all(bind=test_engine)
     with TestClient(app) as test_client:
         yield test_client
+    Base.metadata.drop_all(bind=test_engine)
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def db():
     db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-@pytest.fixture(scope="function", autouse=True)
-def clean_db():
-    """Очищает все таблицы после каждого теста"""
-    yield
-    for table in reversed(Base.metadata.sorted_tables):
-        test_engine.execute(table.delete())
 
 def pytest_sessionfinish(session, exitstatus):
     try:
