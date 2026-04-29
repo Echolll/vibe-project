d071fd206fe0b7d3f3ec8dce11c2a49d79b97bd3
vera <elcheva_v_a@bk.ru>
2026-04-29 11:23:38 +0300

    Revert "-создала папку services, где будет находиться логика приложения"
    This reverts commit d53893cf11b2f94f863896371505a8874fd6b8ee.

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,7 +1,11 @@
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
+
+from backend.app.database import get_db,Users
 
 SECRET_KEY = "DEV_SECRET_KEY_123" # в реальном проекте должен быть скрыт
 ALGORITHM = "HS256"
@@ -22,3 +26,18 @@ def create_token(data: dict):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
+
+def get_current_user(db: Session = Depends(get_db),
+                     token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Невалидный токен")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Токен просрочен или изменен")
+
+    user = db.query(Users).filter(Users.id == int(user_id)).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Пользователь не найден")
+    return user
