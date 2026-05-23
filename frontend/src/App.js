2d1cba08d2f18771676ae10755eb82d226f6a0f1
Milana <milanakokovina@yandex.ru>
2026-05-23 14:53:30 +0300

    обновила роутинг (добавила ManageParticipantsPage, исправила импорты)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import EventsPage from './pages/EventsPage';
 import EventDetailPage from './pages/EventDetailPage';
-import CreateEventPage from './pages/CreateEventPage';  // ← добавить
+import CreateEventPage from './pages/CreateEventPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import ManageParticipantsPage from './pages/ManageParticipantsPage';
 import './App.css';
 
 function App() {
@@ -12,7 +15,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<EventsPage />} />
         <Route path="/events/:id" element={<EventDetailPage />} />
-        <Route path="/create" element={<CreateEventPage />} />  // ← добавить
+        <Route path="/events/:id/manage" element={<ManageParticipantsPage />} />
+        <Route path="/create" element={<CreateEventPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
